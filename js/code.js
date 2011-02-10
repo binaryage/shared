6f029dea6c00de7ece48b65456fd22c823d352fc
Antonin Hildebrand <antonin@hildebrand.cz>
2011-02-10 23:30:13 +0100

    tracking with state, fixed share buttons in chrome

--- js/code.js
+++ js/code.js
@@ -117,6 +117,127 @@ e(this);b=new u(c.find(":input"),c,a);c.data("validator",b)});else{b=new u(this,
 
 /**
  * @license 
+ * jQuery Tools 1.2.5 History "Back button for AJAX apps"
+ * 
+ * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
+ * 
+ * http://flowplayer.org/tools/toolbox/history.html
+ * 
+ * Since: Mar 2010
+ * Date:    Wed Sep 22 06:02:10 2010 +0000 
+ */
+(function($) {
+		
+	var hash, iframe, links, inited;		
+	
+	$.tools = $.tools || {version: '1.2.5'};
+	
+	$.tools.history = {
+	
+		init: function(els) {
+			
+			if (inited) { return; }
+			
+			// IE
+			if ($.browser.msie && $.browser.version < '8') {
+				
+				// create iframe that is constantly checked for hash changes
+				if (!iframe) {
+					iframe = $("<iframe/>").attr("src", "javascript:false;").hide().get(0);
+					$("body").append(iframe);
+									
+					setInterval(function() {
+						var idoc = iframe.contentWindow.document, 
+							 h = idoc.location.hash;
+					
+						if (hash !== h) {						
+							$.event.trigger("hash", h);
+						}
+					}, 100);
+					
+					setIframeLocation(location.hash || '#');
+				}
+
+				
+			// other browsers scans for location.hash changes directly without iframe hack
+			} else { 
+				setInterval(function() {
+					var h = location.hash;
+					if (h !== hash) {
+						$.event.trigger("hash", h);
+					}						
+				}, 100);
+			}
+
+			links = !links ? els : links.add(els);
+			
+			els.click(function(e) {
+				var href = $(this).attr("href");
+				
+				var firstHref = $(this).parents('ul').children().eq(0).find('a').attr('href');
+				if (href == firstHref) {
+				    href = "#";
+				}
+
+				if (window.SSTracker) {
+				    SSTracker.setState(href.substring(1));
+				}
+			    
+				if (iframe) { setIframeLocation(href); }
+				
+				location.href = href; // HACK: woid
+				
+				
+				// handle non-anchor links
+				if (href.slice(0, 1) != "#") {
+					location.href = "#" + href;
+					return e.preventDefault();		
+				}
+				
+			}); 
+			
+			inited = true;
+		}	
+	};  
+	
+
+	function setIframeLocation(h) {
+		if (h) {
+			var doc = iframe.contentWindow.document;
+			doc.open().close();	
+			doc.location.hash = h;
+		}
+	} 
+		 
+	// global histroy change listener
+	$(window).bind("hash", function(e, h)  { 
+		if (h) {
+			links.filter(function() {
+			  var href = $(this).attr("href");
+			  return href == h || href == h.replace("#", ""); 
+			}).trigger("history", [h]);	
+		} else {
+			links.eq(0).trigger("history", [h]);	
+		}
+
+		hash = h;
+
+	});
+		
+	
+	// jQuery plugin implementation
+	$.fn.history = function(fn) {
+
+		$.tools.history.init(this);
+
+		// return jQuery
+		return this.bind("history", fn);		
+	};	
+		
+})(jQuery); 
+
+/**
+ * @license 
  * jQuery Tools 1.2.5 Overlay - Overlay base. Extend it.
  * 
  * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
@@ -228,6 +349,10 @@ e(this);b=new u(c.find(":input"),c,a);c.data("validator",b)});else{b=new u(this,
 				// can be opened only once
 				if (self.isOpened()) { return self; }
 				
+				if (window.SSTracker) {
+				    SSTracker.pushState(trigger.attr('id'));
+				}
+				
 				// find the effect
 		 		var eff = effects[conf.effect];
 		 		if (!eff) { throw "Overlay: cannot find effect : \"" + conf.effect + "\""; }
@@ -325,6 +450,10 @@ e(this);b=new u(c.find(":input"),c,a);c.data("validator",b)});else{b=new u(this,
 				fire.trigger(e);				
 				if (e.isDefaultPrevented()) { return; }				
 				
+				if (window.SSTracker) {
+				    SSTracker.popState();
+				}
+
 				opened = false;
 				
 				// close effect
@@ -706,12 +835,13 @@ e(this);b=new u(c.find(":input"),c,a);c.data("validator",b)});else{b=new u(this,
 				//var padd = parseInt(info.css("paddingLeft"), 10) +  parseInt(info.css("paddingRight"), 10);
 				info.html(text); //.css({width: width - padd});
 				
-                // overlay.css({
-                //     width: width, height: image.height, left: left
-                // });
                 
-                
-                
+                if (!conf.preventResize) {
+                    console.log('x');
+                    overlay.css({
+                        width: width, height: image.height, left: left
+                    });
+                }
                 
 				els.show(); close.show();
 				
